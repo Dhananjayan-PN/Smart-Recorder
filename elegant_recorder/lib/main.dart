2fc43913e1ae4c7d263fe5528e6da3cc1480e977
Dhananjayan P N <pndjay@gmail.com>
2020-04-24 18:34:25 +0530

    layout

--- elegant_recorder/lib/main.dart
+++ elegant_recorder/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,20 +9,128 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: GradientAppBar(
+          leading: Padding(
+            padding: EdgeInsets.only(left: 20, top: 5),
+            child: Icon(
+              Icons.mic,
+              size: 30,
+            ),
+          ),
+          elevation: 30,
+          gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Color(0xff000428), Color(0xff004e92)]),
+          title: Text(
+            'Elegant Recorder',
+            style: TextStyle(fontSize: 20),
+          ),
+          actions: <Widget>[
+            PopupMenuButton(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 1,
+                  child: Text('Settings'),
+                ),
+                PopupMenuItem(
+                  value: 2,
+                  child: Text('Rate the app'),
+                )
+              ],
+              onSelected: (value) {
+                if (value == 1) {
+                  Navigator.push(context, PageTransition(type: PageTransitionType.rightToLeft, child: Settings()));
+                }
+                if (value == 2) {}
+              },
+            )
+          ],
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Record',
+              ),
+              Tab(
+                text: 'Recordings',
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Record(),
+            Recordings(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Record extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(begin: Alignment.bottomCenter, end: Alignment.topCenter, colors: [Color(0xff000428), Color(0xff004e92)]),
+      ),
+    );
+  }
+}
+
+class Recordings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(begin: Alignment.bottomCenter, end: Alignment.topCenter, colors: [Color(0xff000428), Color(0xff004e92)]),
+      ),
+    );
+  }
+}
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: GradientAppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        elevation: 30,
+        gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Color(0xff000428), Color(0xff004e92)]),
+        title: Text(
+          'Settings',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomCenter, end: Alignment.topCenter, colors: [Color(0xff000428), Color(0xff004e92)]),
+        ),
+      ),
+    );
   }
 }
